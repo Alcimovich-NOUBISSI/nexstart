447443e31e1e38bd09590caf9ee57e1340261571
Alcimovich <alcimovichnoubissi@gmail.com>
2024-03-01 23:40:25 +0100

    first commit

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ $(document).ready(function(){
     $(this).siblings().removeClass("active");
     });
 });
+
+// window.onscroll = function() {myFunction()};
+
+// // Get the navbar
+// var navbar = document.getElementById("navbar");
+
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//     console.log(window.scrollY);
+//   if (window.scrollY >= 0) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
         
